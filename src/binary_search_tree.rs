671b95f39cb959fbb9d4b426c3f0d704a67a581f
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-19 10:39:30 +0000

    Impl add_node and Node::new

---
+++ src/binary_search_tree.rs
@@ -0,0 +1,68 @@
+mod binary_search_tree;
+/* REQUIREMENTS
+1. Store IoT device objects (containing the IP address, numerical name, and type)
+2. Retrieve IoT objects by numerical name
+3. Iterate over IoT objects
+*/
+
+use std::mem;
+type Tree = Option<Box<Node>>;
+pub struct Node {
+    pub device: IotDevice,
+    left: Tree,
+    right: Tree,
+}
+// the tree is a pointer to the root node
+pub struct BinarySearchTree {
+    root: Tree,
+    pub length: u32,
+}
+
+// IOT device object
+pub struct IotDevice {
+    pub id: u32,
+    pub address: String,
+}
+
+impl Node {
+    // creates a new node
+    pub fn new(device: IotDevice) -> Tree {
+        Some(Box::new(Node {
+            device,
+            left: None,
+            right: None,
+        }))
+    }
+}
+
+// add device to tree
+impl BinarySearchTree {
+    pub fn new() -> BinarySearchTree {
+        BinarySearchTree {
+            root: None,
+            length: 0,
+        }
+    }
+    pub fn add(&mut self, device: IotDevice) {
+        // set the root of the tree to the new node using take
+        // similar to the linked list example
+        let root = mem::replace(&mut self.root, None);
+        self.root = self.add_node(root, device);
+        // increase length
+        self.length += 1;
+    }
+    pub fn add_node(&mut self, node: Tree, device: IotDevice) -> Tree {
+        match node {
+            Some(mut n) => {
+                if n.device.id < n.device.id {
+                    n.left = self.add_node(n.left, device);
+                    Some(n)
+                } else {
+                    n.right = self.add_node(n.right, device);
+                    Some(n)
+                }
+            }
+            _ => Node::new(device),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod binary_search_tree;
+fn main() {
+    let tree = binary_search_tree::BinarySearchTree::new();
+}
